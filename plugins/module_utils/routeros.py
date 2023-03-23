c1a6ae86587d292a01920494f7a2547ef6e2c5e6
Sam Grimee - LX2SG <sgrimee@users.noreply.github.com>
2023-03-23 21:25:23 +0100

    Workaround prompt issues between ROS 6.49 and 7.1.5  (#62) (#161)
    * Prompt support for ROS 6.49+ (#62)
    
    * More precise version match (#62)
    
    * Add changelog fragment and lint (#62)
    
    * Update changelogs/fragments/161-workaround-prompt-with-space.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Vendor version package to support older ansible versions (#62)
    
    * Update plugins/module_utils/routeros.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/module_utils/routeros.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * move PSF-2.0.txt to LICENSES folder
    
    ---------
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/module_utils/routeros.py
+++ plugins/module_utils/routeros.py
@@ -9,6 +9,7 @@ import json
 from ansible.module_utils.common.text.converters import to_native
 from ansible.module_utils.basic import env_fallback
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import to_list, ComplexList
+from ansible_collections.community.routeros.plugins.module_utils.version import LooseVersion
 from ansible.module_utils.connection import Connection, ConnectionError
 
 _DEVICE_CONFIGS = {}
@@ -103,6 +104,16 @@ def to_commands(module, commands):
     return transform(commands)
 
 
+def should_add_leading_space(module):
+    """Determines whether adding a leading space to the command is needed
+    to workaround prompt bug in 6.49 <= ROS < 7.2"""
+    capabilities = get_capabilities(module)
+    network_os_version = capabilities.get('device_info', {}).get('network_os_version')
+    if network_os_version is None:
+        return False
+    return LooseVersion('6.49') <= LooseVersion(network_os_version) < LooseVersion('7.2')
+
+
 def run_commands(module, commands, check_rc=True):
     responses = list()
     connection = get_connection(module)
@@ -116,6 +127,9 @@ def run_commands(module, commands, check_rc=True):
             command = cmd
             prompt = None
             answer = None
+
+        if should_add_leading_space(module):
+            command = " " + command
 
         try:
             out = connection.get(command, prompt, answer)
