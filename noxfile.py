6655fae4771d0914ed6a68789389ae39e605a317
Felix Fontein <felix@fontein.de>
2025-04-19 12:51:43 +0200

    Use antsibull-nox for CI.

---
+++ noxfile.py
@@ -0,0 +1,50 @@
+
+# The following metadata allows Python runners and nox to install the required
+# dependencies for running this Python script:
+#
+# /// script
+# dependencies = ["nox>=2025.02.09", "antsibull-nox"]
+# ///
+
+import os
+import sys
+
+import nox
+
+
+# We try to import antsibull-nox, and if that doesn't work, provide a more useful
+# error message to the user.
+try:
+    import antsibull_nox
+except ImportError:
+    print("You need to install antsibull-nox in the same Python environment as nox.")
+    sys.exit(1)
+
+
+IN_CI = os.environ.get("CI") == "true"
+
+
+antsibull_nox.load_antsibull_nox_toml()
+
+
+@nox.session(name="update-docs", default=True)
+def update_docs_fragments(session: nox.Session) -> None:
+    """
+    Update/check auto-generated parts of docs fragments.
+    """
+    session.install("ansible-core")
+    prepare = antsibull_nox.sessions.prepare_collections(
+        session, install_in_site_packages=True
+    )
+    if not prepare:
+        return
+    data = ["python", "tests/update-docs.py"]
+    if IN_CI:
+        data.append("--lint")
+    session.run(*data)
+
+
+# Allow to run the noxfile with `python noxfile.py`, `pipx run noxfile.py`, or similar.
+# Requires nox >= 2025.02.09
+if __name__ == "__main__":
+    nox.main()
--- tests/update-docs.py
+++ tests/update-docs.py
@@ -11,9 +11,6 @@ Updates DOCUMENTATION of modules using module_utils._api_data with the correct l
 
 import sys
 
-# Ensure that we can import things from ansible_collections
-sys.path.append('../../..')
-
 from ansible_collections.community.routeros.plugins.module_utils._api_data import (
     PATHS,
     join_path,
@@ -26,24 +23,34 @@ MODULES = [
 ]
 
 
-def update_file(file, begin_line, end_line, choice_line, path_choices):
+def update_file(file: str, begin_line: str, end_line: str, choice_line: str, path_choices: list[str]) -> bool:
     with open(file, 'r', encoding='utf-8') as f:
         lines = f.read().splitlines()
     begin_index = lines.index(begin_line)
     end_index = lines.index(end_line, begin_index + 1)
     new_lines = lines[:begin_index + 1] + [choice_line.format(choice=choice) for choice in path_choices] + lines[end_index:]
-    if lines != new_lines:
-        print(f'{file} has been updated')
-        with open(file, 'w', encoding='utf-8') as f:
-            f.write('\n'.join(new_lines) + '\n')
+    if lines == new_lines:
+        return False
+    print(f'{file} has been updated')
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(new_lines) + '\n')
+    return True
 
 
-def main():
+def main(args: list[str]) -> int:
     path_choices = sorted([join_path(path) for path, path_info in PATHS.items() if path_info.fully_understood])
 
+    changes = False
     for file in MODULES:
-        update_file(file, '    # BEGIN PATH LIST', '    # END PATH LIST', '      - {choice}', path_choices)
+        changes |= update_file(file, '    # BEGIN PATH LIST', '    # END PATH LIST', '      - {choice}', path_choices)
+
+    lint = "--lint" in args
+    if not lint or not changes:
+        return 0
+
+    print("Run 'nox -Re update-docs'!")
+    return 1
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main(sys.argv[1:]))
