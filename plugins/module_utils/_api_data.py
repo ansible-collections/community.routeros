39c06925040b1b16b513b46e1411fc4fa9418067
Christian Henriksen <unicorn@0xunicorn.com>
2025-09-26 17:35:31 +0200

    Add support for VRF to API 'system/logging/action' from v7.19

--- plugins/module_utils/_api_data.py
+++ plugins/module_utils/_api_data.py
@@ -5251,6 +5251,7 @@ PATHS = {
                 ([('7.18', '>=')], 'remote-log-format', KeyInfo(default='default')),
                 ([('7.18', '>=')], 'remote-protocol', KeyInfo(default='udp')),
                 ([('7.18', '>=')], 'cef-event-delimiter', KeyInfo(default='\r\n')),
+                ([('7.19', '>=')], 'vrf', KeyInfo(default='main')),
             ],
             fields={
                 'bsd-syslog': KeyInfo(default=False),
